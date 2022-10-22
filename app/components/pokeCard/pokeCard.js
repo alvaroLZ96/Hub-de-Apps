8f8ade1af846f472a81e12e98b95825906e677e7
alvaroLZ96 <alvarost54@gmail.com>
2022-10-22 20:22:47 +0200

    feat: formato carta

--- app/components/pokeCard/pokeCard.js
+++ app/components/pokeCard/pokeCard.js
@@ -3,12 +3,20 @@ export const callPokemonCard = (pokemon) => {
   const divPrincipal = document.body.querySelector("#app");
   (divPrincipal.innerHTML = ""),
     (divPrincipal.innerHTML = `
-            <div class="pokemonCardDiv">
-                <h2>${pokemon.name}</h2>
-                <h3>Experiencia base: ${pokemon.experience}</h3>
-                <h3>Número Pokemon: ${pokemon.id}</h3>
-                <h3>Altura: ${pokemon.height}m</h3>
-                <h3>Peso: ${pokemon.weight}kg</h3>
-                <img class="${pokemon.name}" src=${pokemon.image}>
-            </div>`);
+      <div class="pokeBox">
+        <div class="pokemonCardDiv">
+            <div class="box1">
+              <img class="${pokemon.name} poke-img" src=${pokemon.image}>
+            </div>
+            <div class="box2">
+              
+                    <h2>${pokemon.name}</h2>
+                    <h3>Experiencia base: ${pokemon.experience}<span>&#160;</span></h3>
+                    <h3>Número Pokemon: ${pokemon.id}<span>&#160;</span></h3>
+                    <h3>Altura: ${pokemon.height}m<span>&#160;</span></h3>
+                    <h3>Peso: ${pokemon.weight}kg<span>&#160;</span></h3>
+                    
+            </div>
+        </div>
+      </div> `);
 };

056ce4a00c6e885bda68da95aea7b6f9f01e46af
alvaroLZ96 <113988129+alvaroLZ96@users.noreply.github.com>
2023-03-06 20:23:52 +0100

    Update nav.js

--- app/components/nav/nav.js
+++ app/components/nav/nav.js
@@ -2,7 +2,6 @@ import "./style.css";
 import { randomColor } from "../../utils/theme-color";
 
 export const changeTheme = () => {
-  /* document.body.classList.toggle("dark");//meter en lugar de dark una funcion k me cambie el tema de manera random */
   const themeBtn = document.createElement("button");
   themeBtn.innerText = "🌈";
   themeBtn.id = "themeBtn";
